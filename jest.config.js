9dea356ab1918ca1c96178343aa86a0718a31daa
Mikalai Lazitski <mlazitski@elinext.com>
2023-06-03 16:45:12 +0300

    feat: add simple tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  testMatch: ["**/*.test.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "/opt/nodejs/products": "<rootDir>/src/layers/mock/nodejs/products",
+  },
 };
--- test/products.test.ts
+++ test/products.test.ts
@@ -1,18 +1,38 @@
-import { APIGatewayProxyEvent } from "aws-lambda"
-import { handler } from "../src/products/getList/getList"
+import { handler as getProductList } from "../src/products/getList/getList"
+import { handler as getProductById } from "../src/products/getById/getById"
 import { getMock } from '../src/layers/mock/nodejs/products';
 
 
 describe('Products', () => {
-    let event = null as unknown as APIGatewayProxyEvent
-    beforeAll(() => {
+    let event: any;
 
-    })
     describe('Get Products List', () => {
-        it('', async () => {
-            const responce = await handler(event)
+        it('should have correct responce', async () => {
+            const responce = await getProductList(event)
             expect(responce.statusCode).toBe(200);
             expect(responce.body).toBe(JSON.stringify(getMock()))
+        })
+    })
+    describe('Get Products By Id', () => {
+        beforeEach(() => {
+            event = {
+                pathParameters: {
+                    product: ''
+                }
+            };
+        })
+        it('should get first product', async () => {
+            const expectedProduct = getMock()[0]
+            event.pathParameters.product = expectedProduct.id
+            const responce = await getProductById(event)
+            expect(responce.statusCode).toBe(200);
+            expect(responce.body).toBe(JSON.stringify(expectedProduct))
+        })
+        it('should return "Product not found"', async () => {
+            const expectedResponce = "Product not found"
+            const responce = await getProductById(event)
+            expect(responce.statusCode).toBe(400);
+            expect(responce.body).toBe(expectedResponce)
         })
     })
 })
