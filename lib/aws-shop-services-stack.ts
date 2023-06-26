0ee69617178253f60551b989e66bba47b771aede
Mikalai Lazitski <mlazitski@elinext.com>
2023-06-26 20:16:12 +0300

    feat: add sns and sqs stacks

--- lib/aws-shop-services-stack.ts
+++ lib/aws-shop-services-stack.ts
@@ -3,13 +3,19 @@ import { Construct } from 'constructs';
 import { ProductsServiceStack } from './products-service-stack';
 import { GatewayStack } from './gateway-shop-stack';
 import { ImportServiceStack } from './import-service-stack';
+import { SqsStack } from './sqs-service-stack';
+import { SnsStack } from './sns-service-stack';
+import 'dotenv/config'
 
 export class AwsShopServicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+
     const gateway = new GatewayStack(this, 'GatewayService')
-    new ProductsServiceStack(this, 'ProductsService', gateway.api)
-    new ImportServiceStack(this, 'ImportService', gateway.api)
+    const sqs = new SqsStack(this, 'SqsService')
+    const sns = new SnsStack(this, 'SnsService', process.env.SNS_EMAIL as string)
+    new ProductsServiceStack(this, 'ProductsService', gateway.api, sqs.catalogItemsQueue, sns.createProductTopic)
+    new ImportServiceStack(this, 'ImportService', gateway.api, sqs.catalogItemsQueue)
   }
 }
